e1321e416e6de5c53ce097711ab0117df11cdbe3
KoukiNishihara <e175774@ie.u-ryukyu.ac.jp>
2020-03-06 14:39:26 +0900

    add personal model

--- lite/examples/image_classification/ios/ImageClassification/ModelDataHandler/ModelDataHandler.swift
+++ lite/examples/image_classification/ios/ImageClassification/ModelDataHandler/ModelDataHandler.swift
@@ -34,7 +34,7 @@ typealias FileInfo = (name: String, extension: String)
 
 /// Information about the MobileNet model.
 enum MobileNet {
-  static let modelInfo: FileInfo = (name: "mobilenet_quant_v1_224", extension: "tflite")
+  static let modelInfo: FileInfo = (name: "retrained_graph", extension: "tflite")
   static let labelsInfo: FileInfo = (name: "labels", extension: "txt")
 }
 
@@ -55,8 +55,8 @@ class ModelDataHandler {
 
   let batchSize = 1
   let inputChannels = 3
-  let inputWidth = 224
-  let inputHeight = 224
+  let inputWidth = 299
+  let inputHeight = 299
 
   // MARK: - Private Properties
 
